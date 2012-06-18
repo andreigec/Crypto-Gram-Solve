55cd65922c1ddd610cd5d1ea557aff98f740f840
Andrei Gec <andreigec@hotmail.com>
2012-06-19 09:55:36 +1000

    1

---
+++ SOURCE/CryptoSolve/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace CryptoSolve
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopAfterFirstResultToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.encryptedtext = new System.Windows.Forms.TextBox();
+            this.loadbutton = new System.Windows.Forms.Button();
+            this.savebutton = new System.Windows.Forms.Button();
+            this.decodebutton = new System.Windows.Forms.Button();
+            this.decryptedtext = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statuslabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.threadcheck = new System.Windows.Forms.Timer(this.components);
+            this.encodebutton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Silver;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.optionsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(476, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stopAfterFirstResultToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // stopAfterFirstResultToolStripMenuItem
+            // 
+            this.stopAfterFirstResultToolStripMenuItem.Name = "stopAfterFirstResultToolStripMenuItem";
+            this.stopAfterFirstResultToolStripMenuItem.Size = new System.Drawing.Size(187, 22);
+            this.stopAfterFirstResultToolStripMenuItem.Text = "Stop After First Result";
+            this.stopAfterFirstResultToolStripMenuItem.Click += new System.EventHandler(this.stopAfterFirstResultToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem1});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // helpToolStripMenuItem1
+            // 
+            this.helpToolStripMenuItem1.Name = "helpToolStripMenuItem1";
+            this.helpToolStripMenuItem1.Size = new System.Drawing.Size(99, 22);
+            this.helpToolStripMenuItem1.Text = "Help";
+            this.helpToolStripMenuItem1.Click += new System.EventHandler(this.helpToolStripMenuItem1_Click);
+            // 
+            // encryptedtext
+            // 
+            this.encryptedtext.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.encryptedtext.Location = new System.Drawing.Point(6, 19);
+            this.encryptedtext.Multiline = true;
+            this.encryptedtext.Name = "encryptedtext";
+            this.encryptedtext.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.encryptedtext.Size = new System.Drawing.Size(445, 127);
+            this.encryptedtext.TabIndex = 1;
+            this.encryptedtext.WordWrap = false;
+            this.encryptedtext.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.encryptedtext_KeyPress);
+            // 
+            // loadbutton
+            // 
+            this.loadbutton.Location = new System.Drawing.Point(12, 27);
+            this.loadbutton.Name = "loadbutton";
+            this.loadbutton.Size = new System.Drawing.Size(75, 23);
+            this.loadbutton.TabIndex = 2;
+            this.loadbutton.Text = "Load";
+            this.loadbutton.UseVisualStyleBackColor = true;
+            this.loadbutton.Click += new System.EventHandler(this.loadbutton_Click);
+            // 
+            // savebutton
+            // 
+            this.savebutton.Location = new System.Drawing.Point(93, 27);
+            this.savebutton.Name = "savebutton";
+            this.savebutton.Size = new System.Drawing.Size(75, 23);
+            this.savebutton.TabIndex = 3;
+            this.savebutton.Text = "Save";
+            this.savebutton.UseVisualStyleBackColor = true;
+            this.savebutton.Click += new System.EventHandler(this.savebutton_Click);
+            // 
+            // decodebutton
+            // 
+            this.decodebutton.Location = new System.Drawing.Point(7, 214);
+            this.decodebutton.Name = "decodebutton";
+            this.decodebutton.Size = new System.Drawing.Size(156, 23);
+            this.decodebutton.TabIndex = 4;
+            this.decodebutton.Text = "Decode Encrypted Text";
+            this.decodebutton.UseVisualStyleBackColor = true;
+            this.decodebutton.Click += new System.EventHandler(this.decodebutton_Click);
+            // 
+            // decryptedtext
+            // 
+            this.decryptedtext.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.decryptedtext.Location = new System.Drawing.Point(6, 19);
+            this.decryptedtext.Multiline = true;
+            this.decryptedtext.Name = "decryptedtext";
+            this.decryptedtext.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.decryptedtext.Size = new System.Drawing.Size(445, 349);
+            this.decryptedtext.TabIndex = 5;
+            this.decryptedtext.WordWrap = false;
+            this.decryptedtext.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.decryptedtext_KeyPress);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel2,
+            this.statuslabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 620);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(476, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(42, 17);
+            this.toolStripStatusLabel2.Text = "Status:";
+            // 
+            // statuslabel
+            // 
+            this.statuslabel.Name = "statuslabel";
+            this.statuslabel.Size = new System.Drawing.Size(12, 17);
+            this.statuslabel.Text = "x";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.encryptedtext);
+            this.groupBox1.Location = new System.Drawing.Point(7, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(457, 152);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cryptogram(Encrypted Text)";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.decryptedtext);
+            this.groupBox2.Location = new System.Drawing.Point(7, 243);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(457, 374);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Decrypted Text";
+            // 
+            // threadcheck
+            // 
+            this.threadcheck.Tick += new System.EventHandler(this.threadcheck_Tick);
+            // 
+            // encodebutton
+            // 
+            this.encodebutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.encodebutton.Location = new System.Drawing.Point(308, 214);
+            this.encodebutton.Name = "encodebutton";
+            this.encodebutton.Size = new System.Drawing.Size(156, 23);
+            this.encodebutton.TabIndex = 9;
+            this.encodebutton.Text = "Encode Decrypted Text";
+            this.encodebutton.UseVisualStyleBackColor = true;
+            this.encodebutton.Click += new System.EventHandler(this.encodebutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 642);
+            this.Controls.Add(this.encodebutton);
+            this.Controls.Add(this.loadbutton);
+            this.Controls.Add(this.decodebutton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.savebutton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox encryptedtext;
+        private System.Windows.Forms.Button loadbutton;
+        private System.Windows.Forms.Button savebutton;
+        private System.Windows.Forms.Button decodebutton;
+        private System.Windows.Forms.TextBox decryptedtext;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statuslabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.Timer threadcheck;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopAfterFirstResultToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem1;
+        private System.Windows.Forms.Button encodebutton;
+    }
+}
+
---
+++ SOURCE/CryptoSolve/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using ANDREICSLIB;
+
+namespace CryptoSolve
+{
+    public partial class Form1 : Form
+    {
+        #region licensing
+
+        private const string AppTitle = "CryptoGramSolve";
+        private const double AppVersion = 0.2;
+        private const String HelpString = "";
+
+        private const String UpdatePath = "https://github.com/EvilSeven/Crypto-Solve/zipball/master";
+        private const String VersionPath = "https://raw.github.com/EvilSeven/Crypto-Solve/master/INFO/version.txt";
+        private const String ChangelogPath = "https://raw.github.com/EvilSeven/Crypto-Solve/master/INFO/changelog.txt";
+
+        private readonly String OtherText =
+            @"©" + DateTime.Now.Year +
+            @" Andrei Gec (http://www.andreigec.net)
+
+Licensed under GNU LGPL (http://www.gnu.org/)
+
+Zip Assets © SharpZipLib (http://www.sharpdevelop.net/OpenSource/SharpZipLib/)
+";
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool ThreadActive()
+        {
+            return (t != null && t.IsAlive);
+        }
+
+        public delegate void IncrementCounter(String v);
+        public delegate bool ResultCallBack(String s);
+
+        public DateTime? LastUpdate = null;
+
+        private Thread t = null;
+
+        public bool GetResultIsOK(String s)
+        {
+            if (string.IsNullOrWhiteSpace(decryptedtext.Text) == false)
+                decryptedtext.Text += "\r\n";
+
+            decryptedtext.Text += s;
+            return stopAfterFirstResultToolStripMenuItem.Checked;
+        }
+
+        public void IncrementProgress(String v)
+        {
+            if (LastUpdate == null || (DateTime.Now - (DateTime)LastUpdate).TotalMilliseconds > 500)
+            {
+                LastUpdate = DateTime.Now;
+            }
+            else
+                return;
+
+            statuslabel.Text = v;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Licensing.CreateLicense(this, HelpString, AppTitle, AppVersion, OtherText, VersionPath, UpdatePath, ChangelogPath, menuStrip1);
+            Solver.Init();
+            statuslabel.Text = "";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void setbuttons(bool running)
+        {
+            if (running)
+            {
+                decodebutton.Text = "Abort";
+            }
+            else
+            {
+                decodebutton.Text = "Decode Cryptogram";
+                statuslabel.Text = "";
+            }
+        }
+
+        private void Solve()
+        {
+            if (ThreadActive())
+                t = null;
+
+            if (t != null)
+                return;
+
+            statuslabel.Text = "0";
+
+            t = new Thread(() => Solver.Solve(encryptedtext.Text, GetResultIsOK, IncrementProgress));
+            t.Start();
+            threadcheck.Enabled = true;
+        }
+
+        private void decodebutton_Click(object sender, EventArgs e)
+        {
+            if (ThreadActive())
+                abort();
+            else
+                Solve();
+        }
+
+        private void loadbutton_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "|*.txt";
+            var res = ofd.ShowDialog();
+            if (res != DialogResult.OK)
+                return;
+
+            LoadFile(ofd.FileName);
+        }
+
+        private void LoadFile(String fn)
+        {
+            var t = FileUpdates.LoadFile(fn);
+
+            var s1=t.Split(new[] {'\f'}, StringSplitOptions.None);
+            if (s1.Count() == 0)
+            {
+                MessageBox.Show("Error loading file");
+                return;
+            }
+
+            encryptedtext.Text = s1[0];
+
+            if (s1.Count()>=1)
+            decryptedtext.Text = s1[1];
+        }
+
+        private void SaveToFile(String filename)
+        {
+            var o = encryptedtext.Text + '\f' + decryptedtext.Text;
+            FileUpdates.SaveToFile(filename,o);
+        }
+
+        private void threadcheck_Tick(object sender, EventArgs e)
+        {
+            if (ThreadActive() == false)
+            {
+                t = null;
+                threadcheck.Enabled = false;
+            }
+
+            setbuttons(t != null);
+            setbuttons(t != null);
+        }
+
+        private void abort()
+        {
+            if (ThreadActive())
+            {
+                t.Abort();
+                t = null;
+            }
+
+            setbuttons(false);
+        }
+
+        private void stopAfterFirstResultToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            stopAfterFirstResultToolStripMenuItem.Checked = !stopAfterFirstResultToolStripMenuItem.Checked;
+        }
+
+        private void helpToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            var helpstr = @"Help:
+";
+
+            MessageBox.Show(helpstr, "Help", MessageBoxButtons.OK);
+        }
+
+        private void savebutton_Click(object sender, EventArgs e)
+        {
+            var sfd = new SaveFileDialog();
+            sfd.Filter = "|*.txt";
+            var res = sfd.ShowDialog();
+            if (res != DialogResult.OK)
+                return;
+            SaveToFile(sfd.FileName);
+        }
+
+        
+
+        private void encodebutton_Click(object sender, EventArgs e)
+        {
+            var et = Solver.EncodeText(decryptedtext.Text);
+            encryptedtext.Text = et;
+        }
+
+        private void decryptedtext_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled=TextboxUpdates.HandleInput(TextboxUpdates.InputType.CreateAllTrue(), e.KeyChar, ref decryptedtext);
+        }
+
+        private void encryptedtext_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = TextboxUpdates.HandleInput(TextboxUpdates.InputType.CreateAllTrue(), e.KeyChar, ref encryptedtext);
+        }
+    }
+}
